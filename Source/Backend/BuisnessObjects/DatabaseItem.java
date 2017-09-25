ea300493ada01d1f096f777c33959c05f6acfd32
UnclePat <patrik_enderle@web.de>
2017-09-25 12:50:40 +0200

    DatabaseItem Abstraktion eingeführt

--- Source/Backend/BuisnessObjects/DatabaseItem.java
+++ Source/Backend/BuisnessObjects/DatabaseItem.java
@@ -1,4 +1,14 @@
 package Backend.BuisnessObjects;
 
+import java.util.List;
+
 public abstract class DatabaseItem {
+    List<String> columnNames;
+    String tableName;
+
+    public void DatabaseItem(List<String> _columnNames, String _tableName){
+
+        this.columnNames = _columnNames;
+        this.tableName = _tableName;
+    }
 }
--- Source/Backend/BuisnessObjects/Kategorie.java
+++ Source/Backend/BuisnessObjects/Kategorie.java
@@ -1,4 +1,7 @@
 package Backend.BuisnessObjects;
 
+import java.util.List;
+
 public class Kategorie extends DatabaseItem{
+
 }
