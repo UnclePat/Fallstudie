47dae9f79c5dedd797591837680b9ea767830458
EindringlicheKantlesung <dondieselx3@googlemail.com>
2017-10-02 09:43:24 +0200

    Doller Kack

--- Source/Frontend/DeineMudderMachtGerneFrontEnd.java
+++ Source/Frontend/DeineMudderMachtGerneFrontEnd.java
@@ -1,4 +1,88 @@
 package Frontend;
 
-public class DeineMudderMachtGerneFrontEnd {
+import com.intellij.ui.JBColor;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.awt.*;
+
+import static com.sun.glass.ui.Cursor.setVisible;
+import static java.awt.Color.red;
+
+public class DeineMudderMachtGerneFrontEnd extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Dein Haushaltsplan");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER_RIGHT);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("Willkommen");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label userName = new Label("Benutzername:");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label pw = new Label("Passwort:");
+        grid.add(pw, 0, 2);
+
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+
+        Button btn = new Button("Einloggen");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+
+        grid.setStyle("-fx-background-color: red");
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent e) {
+                actiontarget.setFill(Color.FIREBRICK);
+                actiontarget.setText("Sign in button pressed");
+            }
+        });
+
+
+        Scene scene = new Scene(grid, 1000, 500);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+    }
+
+
 }
