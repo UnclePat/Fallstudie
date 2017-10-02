505d485dec738777b3fc9fdf0f6a79e6e3a02b1c
UnclePat <patrik_enderle@web.de>
2017-10-02 09:13:10 +0200

    User Implementierung

--- Source/Backend/User/User.java
+++ Source/Backend/User/User.java
@@ -1,23 +1,35 @@
 package Backend.User;
 
 import Backend.BuisnessObjects.DatabaseItem;
+import Backend.Database.DataBaseServer;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class User extends DatabaseItem{
-    private List<UserGroup> userGroups;
+    private List<UserAccessRight> userGroups;
 
-    public List<UserGroup> getUserGroups() {
+    public List<UserAccessRight> getUserGroups() {
         return userGroups;
     }
 
-    private void setUserGroups(List<UserGroup> _userGroups){
+    private void setUserGroups(List<UserAccessRight> _userGroups){
         this.userGroups = _userGroups;
     }
 
     @Override
     public void saveItem() {
+        try {
+            String query = "";
+            List<String> values = new ArrayList<>();
 
+            DataBaseServer connection = new DataBaseServer();
+
+            connection.update(query, values);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
