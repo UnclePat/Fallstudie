6c39fa0cb60780f7431dd9e5c4d37a72388b1fc9
Lukas <lukas.ascher79@googlemail.com>
2017-10-02 11:49:14 +0200

    db scripts

---
+++ SourceTest/UserklasseTest.java
@@ -0,0 +1,22 @@
+import Backend.User.User;
+
+import java.util.Date;
+
+public class UserklasseTest {
+
+  public void test() {
+    User testUser = new User();
+
+    testUser.setAdmin(true);
+    testUser.setName("Hans");
+    testUser.setPassword("pwd4sa");
+    testUser.setDateCreated(new Date("2017-10-01"));
+    testUser.setFkeyUserCreated(2);
+    testUser.setdeletionFlag(false);
+    testUser.setDeletedByUser(testUser);
+    testUser.setDateDeleted(new Date("2017-10-01"));
+  }
+
+    testUser.saveItem();
+  }
+
