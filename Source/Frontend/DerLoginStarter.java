981ef29b274ec00973d078b30c1607f9c6b75380
EindringlicheKantlesung <dondieselx3@googlemail.com>
2017-10-10 17:25:36 +0200

    Doller Kack

--- Source/Frontend/DerLoginStarter.java
+++ Source/Frontend/DerLoginStarter.java
@@ -1,5 +1,6 @@
 package Frontend;
 
+import Backend.User.UserUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -7,12 +8,16 @@ import javafx.application.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
-    public class DerLoginStarter extends Application {
+
+
+public class DerLoginStarter extends Application {
         @Override
         public void start(Stage primaryStage) {
             try {
@@ -29,11 +34,14 @@ import javafx.scene.Scene;
         }
 
 
+
         @FXML private Button actionTarget;
+        @FXML private Text fehler;
 
         public void handleSubmitButtonaction(ActionEvent actionEvent) {
-
             actionTarget.setStyle("-fx-border-color: blue;");
+
+            fehler.setVisible(true);
 
         }
     }
