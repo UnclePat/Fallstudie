c05153d4b6959efb0977910c6aeafbb6b44fb870
JA4N <jan.lindheimer@hotmail.com>
2017-10-10 12:27:24 +0200

    -Button Angepasst -Login-Logik

--- Source/Frontend/DerLoginStarter.java
+++ Source/Frontend/DerLoginStarter.java
@@ -1,18 +1,26 @@
 package Frontend;
 
+import Backend.Database.DataBaseServer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.application.*;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import java.sql.*;
 
-    public class DerLoginStarter extends Application {
+public class DerLoginStarter extends Application {
+
+        static String userAbfrage;
+        ResultSet resultset;
+        Statement stmt = null;
+        public String unser;
+        String userName;
+        String userPassword;
+
         @Override
         public void start(Stage primaryStage) {
             try {
@@ -24,16 +32,72 @@ import javafx.scene.Scene;
             } catch (Exception e) {
                 e.printStackTrace();
 
+            }//try
 
-            }
-        }
+
+
+        }//start
 
 
         @FXML private Button actionTarget;
+        @FXML private TextField passwortField;
+        @FXML private TextField textField;
 
         public void handleSubmitButtonaction(ActionEvent actionEvent) {
 
             actionTarget.setStyle("-fx-border-color: blue;");
             
+        }
+
+        public void buttonPressed(ActionEvent actionEvent){
+
+            //DB-Verbindung herstellen
+            try {
+                DataBaseServer connection = new DataBaseServer();
+                connection.dbConnect();
+                System.out.println("Verbindung zur DB war erfolgreich");
+            }
+            catch(Exception s) {
+                System.out.println(s.toString() + "Verbindung zur DB war nicht erfolgreich");
+            }//try
+
+            //Login
+            try {
+                //Username aus TextField wird ausgelesen
+               String userTextField = textField.getText();
+               //Passwort aus PasswortField wird ausgelesen
+                String userPasswortField = passwortField.getText();
+
+                //DB inhalt wird ausgelesen
+                userAbfrage = new String ("Select strName, strPassword from Haushaltsbuch");
+                resultset = stmt.executeQuery(userAbfrage);
+
+                while (resultset.next()){
+                    userName = resultset.getString("userName");
+                    userPassword = resultset.getString("userPassword");
+                }
+
+                boolean vergleichUser = userName.equals(userTextField);
+                boolean vergleichPassword = userPassword.equals(userPasswortField);
+
+                if (vergleichUser == true || vergleichPassword == true ){
+                    actionTarget.setStyle("-fx-border-color: green;");
+                    System.out.println("Login war erfolgreich");
+                }
+                else{
+                    actionTarget.setStyle("-fx-border-color: red;");
+                    System.out.println("Login war nicht erfolgreich");
+                }
+
+            }
+            catch(Exception s){
+                actionTarget.setStyle("-fx-border-color: red;");
+                System.out.println(s.toString() + "Login war nicht erfolgreich");
+
+
+            }//try
+
+
+
         }
     }
