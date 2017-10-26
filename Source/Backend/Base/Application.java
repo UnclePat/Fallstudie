984bf26e4b471887e03c0cb934d831507c78c65c
UnclePat <patrik_enderle@web.de>
2017-10-26 12:48:03 +0200

    Error Dialog Login

--- Source/Backend/Base/Application.java
+++ Source/Backend/Base/Application.java
@@ -5,6 +5,9 @@ import Frontend.LoginController;
 import Frontend.MainFormController;
 import javafx.stage.Stage;
 
+/**
+ * Hauptklasse der Anwendung. Dient als Haupteinsprungpunkt.
+ */
 public class Application extends javafx.application.Application {
     private static User currentUser;
 
@@ -16,11 +19,21 @@ public class Application extends javafx.application.Application {
         return Application.currentUser;
     }
 
-    //Startet unsere Applikation und ruft dazu als erstes, den Login Controller mit dem aktuellen Benutzer auf
+    /**
+     * Startet den Hauptthread der Anwendung.
+     * @param args
+     * Argumente für den Start der Applikation
+     */
     public static void main(String[] args) {
         launch(args);
     }
 
+    /**
+     * Lädt den Login Bildschrim und setzt anhand dessen den angemeldeten Benutzer der Applikation. Anschließend wird
+     * das MainForm gestartet.
+     * @param primaryStage
+     * Primäre Stage, die automatisch per FXML generiert wird.
+     */
     @Override
     public void start(Stage primaryStage) {
         LoginController controller = new LoginController();
